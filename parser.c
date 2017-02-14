b663b8614b5e7a9d07f7f503e277981e2a021f6d
jw505 <jw505@duke.edu>
2017-02-13 23:04:20 -0500

    commented free before parse_request return

--- parser.c
+++ parser.c
@@ -4,37 +4,7 @@
 
 //char* buffer = "   CONNECT ftp://www.cmu.edu:8088/hub/index.html HTTP/1.1 lala \r\nthis is a bullshit header\r\nConnection:close\r\n\r\n";
 
-#ifndef HAVE_MEMMEM
 
-#ifndef _LIBC
-# define __builtin_expect(expr, val)   (expr)
-#endif
-
-/* Return the first occurrence of NEEDLE in HAYSTACK.  */
-void *memmem (const void *haystack, size_t haystack_len, const void *needle,size_t needle_len){
-  const char *begin;
-  const char *const last_possible  = (const char *) haystack + haystack_len - needle_len;
-  
-  if (needle_len == 0)
-    /* The first occurrence of the empty string is deemed to occur at
-       the beginning of the string.  */
-    return (void *) haystack;
-  
-  /* Sanity check, otherwise the loop might search through the whole
-     memory.  */
-  if (__builtin_expect (haystack_len < needle_len, 0))
-    return NULL;
-  
-  for (begin = (const char *) haystack; begin <= last_possible; ++begin)
-    if (begin[0] == ((const char *) needle)[0] &&
-	!memcmp ((const void *) &begin[1],
-		 (const void *) ((const char *) needle + 1),
-		 needle_len - 1))
-      return (void *) begin;
-  
-  return NULL;
-}
-#endif
 
 /*extract information from the request_line buffer*/
 /*req_sock = tokens->host, req_serv = tokens->prtc*/
@@ -150,121 +120,136 @@ char* parse_request(char* buffer, req_info* tokens){
 	printf("DEBUG:The url path is incomplete\n");
 	return NULL;
       }
-    }else{
+    }else{/*no explicit port number*/
       char* po_e = strchr(h_s, '/');
 #ifdef DEBUG
       printf("DEBUG: There is no port field in request line, use default\n");
 #endif
-      strncpy(tokens->host, h_s, (int)(po_e - h_s) );
+      /*get the partial url*/
+      char* po_e = strchr(h_s,'/');
+      if(po_e){
+	/*get the partial url from here*/
+	char* pu_s = po_e;
+	char* pu_e = strchr(pu_s, ' ');
+	tokens->p_url = (char*)malloc((int)(pu_e - pu_s) + 1);
+	strncpy(tokens->p_url, pu_s, (int)(pu_e - pu_s)+1);
 #ifdef DEBUG
-      printf("DEBUG:Host is: %s\n",tokens->host);
+	printf("DEBUG: The partial url is:%s\n", tokens->p_url);
 #endif
-      tokens->port = 80;
+	strncpy(tokens->host, h_s, (int)(po_e - h_s) );
 #ifdef DEBUG
-      printf("DEBUG: port int is: %d\n", tokens->port);
+	printf("DEBUG:Host is: %s\n",tokens->host);
 #endif
+	tokens->port = 80;
+#ifdef DEBUG
+	printf("DEBUG: port int is: %d\n", tokens->port);
+#endif
+      }else{
+	printf("DEBUG:The url path is incomplete\n");
+	return NULL;
+      }
     }
   }else{
     printf("DEBUG:Cannot find the start of the host in the request line\n");
     return NULL;
   }
 #ifdef DEBUG
-      printf("DEBUG:Request before rewrite:%s\n", buffer);
+  printf("DEBUG:Request before rewrite:%s\n", buffer);
 #endif
-    /*Need to allocate a new string buffer for new request line with partial url and refilled headers*/
-    char* http = "HTTP/1.1";
-    char* request = (char*)malloc(sizeof(tokens->method) + sizeof(tokens->p_url) + sizeof(http) + sizeof(buffer));
-    if(request == NULL){
-      free(request);
-      printf("Failed to malloc for request\n");
-      return NULL;
-    }
-    //char* ptr = request;
-    /*copy each fragments in request line into new buffer*/
-    strncpy(request, tokens->method,strlen(tokens->method));
+  /*Need to allocate a new string buffer for new request line with partial url and refilled headers*/
+  char* http = "HTTP/1.1";
+  char* request = (char*)malloc(sizeof(tokens->method) + sizeof(tokens->p_url) + sizeof(http) + sizeof(buffer));
+  if(request == NULL){
+    free(request);
+    printf("Failed to malloc for request\n");
+    return NULL;
+  }
+  //char* ptr = request;
+  /*copy each fragments in request line into new buffer*/
+  strncpy(request, tokens->method,strlen(tokens->method));
 #ifdef DEBUG
-    printf("after strncpy method:%s\n",request);
+  printf("after strncpy method:%s\n",request);
 #endif
-    strncpy(request+strlen(tokens->method),tokens->p_url,strlen(tokens->p_url));
+  strncpy(request+strlen(tokens->method),tokens->p_url,strlen(tokens->p_url));
 #ifdef DEBUG
-    printf("after strncpy p_url:%s\n",request);
+  printf("after strncpy p_url:%s\n",request);
 #endif
-    strncpy(request+strlen(tokens->method)+strlen(tokens->p_url),http,strlen(http));
+  strncpy(request+strlen(tokens->method)+strlen(tokens->p_url),http,strlen(http));
 #ifdef DEBUG
-    printf("after strncpy protocol version:%s\n",request);
+  printf("after strncpy protocol version:%s\n",request);
 #endif
-    char* l_e = request+strlen(tokens->method)+strlen(tokens->p_url)+strlen(http);
-
-    if(!l_e){
-      printf("request buffer ran out after first line\n");
+  char* l_e = request+strlen(tokens->method)+strlen(tokens->p_url)+strlen(http);
+  
+  if(!l_e){
+    printf("request buffer ran out after first line\n");
+    return NULL;
+  }else{
+    //printf("DEBUG:l_e is:%s\n", l_e);
+    char* LRCF = "\r\n";
+    // printf("DEBUG: LRCF is:%s\n", LRCF);
+    char* hd_s = strstr(buffer,LRCF);
+    //printf("DEBUG:hd_s is:%s\n", hd_s);
+    if(!hd_s){
+      printf("Request line doesn't end with LRCF\n");
       return NULL;
     }else{
-    //printf("DEBUG:l_e is:%s\n", l_e);
-      char* LRCF = "\r\n";
-      // printf("DEBUG: LRCF is:%s\n", LRCF);
-      char* hd_s = strstr(buffer,LRCF);
-      //printf("DEBUG:hd_s is:%s\n", hd_s);
-      if(!hd_s){
-	printf("Request line doesn't end with LRCF\n");
+      strncpy(l_e, LRCF, 2);//add the first \r\n into new buffer
+      char* end = strstr(buffer,"\r\n\r\n");
+      //printf("DEBUG:end is:%s\n", end);
+      if(!end){
+	printf("The request doesn't end with \\r\\n\n");//
 	return NULL;
       }else{
-	strncpy(l_e, LRCF, 2);//add the first \r\n into new buffer
-	char* end = strstr(buffer,"\r\n\r\n");
-	//printf("DEBUG:end is:%s\n", end);
-	if(!end){
-	  printf("The request doesn't end with \\r\\n\n");
-	  return NULL;
-	}else{
 #ifdef DEBUG
-	  printf("DEBUG:The headers end with \\r\\n\\r\\n\n");
+	printf("DEBUG:The headers end with \\r\\n\\r\\n\n");
 #endif
-	  /*copy the original headers into new buffer*/
-	  char* header = l_e+2;
-	  //printf("DEBUG:header-3 is:%s\n",header-3);
+	/*copy the original headers into new buffer*/
+	char* header = l_e+2;
+	//printf("DEBUG:header-3 is:%s\n",header-3);
 	  //printf("DEBUG:l_e+2-3 is:%s\n",l_e+2-3);
-	  char* buf_header = strstr(buffer,"\r\n");
-	  //printf("DEBUG:buf_header+2 is:'%s'\n",buf_header);
-	  strncpy(header, buf_header+2, (size_t)(end-buf_header-2));
-	  //printf("DEBUG:the copy end-4 is:'%s'\n", end-4);
-	  //printf("DEBUG:after copying whole headers the header is:%s\n", header);
-	}
+	char* buf_header = strstr(buffer,"\r\n");
+	//printf("DEBUG:buf_header+2 is:'%s'\n",buf_header);
+	strncpy(header, buf_header+2, (size_t)(end-buf_header-2));
+	//printf("DEBUG:the copy end-4 is:'%s'\n", end-4);
+	//printf("DEBUG:after copying whole headers the header is:%s\n", header);
+      }
 #ifdef DEBUG
-	printf("after adding rest headers is:%s\n",request);
+      printf("after adding rest headers is:%s\n",request);
 #endif
-	char* hs_in_req = strstr(request,"\r\n");
-	char* l_e1 = request + strlen(request);
-	if(!l_e1){
-	}else{
-	  char* ho_s = strstr(hs_in_req, "Host:");
-	  //	printf("DEBUG:ho_s is:%s\n",ho_s);
-	  if(!ho_s){
-	    char* h_str = "\r\nHost: ";
+      char* hs_in_req = strstr(request,"\r\n");
+      char* l_e1 = request + strlen(request);
+      if(!l_e1){
+      }else{
+	char* ho_s = strstr(hs_in_req, "Host:");
+	//	printf("DEBUG:ho_s is:%s\n",ho_s);
+	if(!ho_s){
+	  char* h_str = "\r\nHost: ";
 #ifdef DEBUG
-	    printf("DEBUG: No Host header field, need to be added as following\n");
-	    printf("Host: %s\n", tokens->host);
+	  printf("DEBUG: No Host header field, need to be added as following\n");
+	  printf("Host: %s\n", tokens->host);
 #endif
-	    strncpy(l_e1, h_str,8);
-	    strncpy(l_e1+8, tokens->host,strlen(tokens->host));
+	  strncpy(l_e1, h_str,8);
+	  strncpy(l_e1+8, tokens->host,strlen(tokens->host));
 #ifdef DEBUG
 	    printf("after adding host request is:%s\n",request);
 #endif
-	  }else{ 
+	}else{ 
 #ifdef DEBUG
-	    printf("DEBUG: Has Host header field, no need to be added\n");
+	  printf("DEBUG: Has Host header field, no need to be added\n");
 #endif
 	  }
-	}
-	char* l_e2 = request + strlen(request);
+      }
+      char* l_e2 = request + strlen(request);
       // printf("DEBUG:request+strlen(request) is:%s\n",l_e2);
-	if(!l_e2){
-	  printf("request buffer ran out\n");
-	  return NULL;
-	}else{
-	  //char*h_str = "\r\nConnection:close";
-	  char* hs_in_req = strstr(request,"\r\n");  
-	  char* cn_s = strstr(hs_in_req,"Connection:");
-	  if((!cn_s) || (cn_s && (*(cn_s - 1) == '-'))){
-	    char*h_str = "\r\nConnection:close"; 
+      if(!l_e2){
+	printf("request buffer ran out\n");
+	return NULL;
+      }else{
+	//char*h_str = "\r\nConnection:close";
+	char* hs_in_req = strstr(request,"\r\n");  
+	char* cn_s = strstr(hs_in_req,"Connection:");
+	if((!cn_s) || (cn_s && (*(cn_s - 1) == '-'))){
+	  char*h_str = "\r\nConnection:close"; 
 #ifdef DEBUG
 	  printf("DEBUG: No Connection header field, need to be added as following\n");
 	  printf("Connection: close\n");
@@ -277,77 +262,77 @@ char* parse_request(char* buffer, req_info* tokens){
 #ifdef DEBUG
 	  printf("DEBUG: Has c-c header field, no need to be added\n");
 #endif
-	  }
 	}
-	char* l_e3 = request + strlen(request);
-	//printf("DEBUG:l_e3 is:%s\n",l_e3);
-	if(!l_e3){
-	  printf("request buffer ran out\n");
-	  return NULL;
+      }
+      char* l_e3 = request + strlen(request);
+      //printf("DEBUG:l_e3 is:%s\n",l_e3);
+      if(!l_e3){
+	printf("request buffer ran out\n");
+	return NULL;
+      }else{
+	char* hs_in_req = strstr(request,"\r\n");
+	char* pc_s = strstr(hs_in_req,"Proxy-Connection:");
+	// printf("DEBUG:pc_s is:%s\n",pc_s);
+	if(!pc_s){
+	  char* h_str = "\r\nProxy-Connection: close"; 
+	  strncpy(l_e3, h_str,strlen(h_str));
+#ifdef DEBUG
+	  printf("DEBUG:after adding p-c:c is:%s\n",request);
+#endif
 	}else{
-	  char* hs_in_req = strstr(request,"\r\n");
-	  char* pc_s = strstr(hs_in_req,"Proxy-Connection:");
-	  // printf("DEBUG:pc_s is:%s\n",pc_s);
-	  if(!pc_s){
-	    char* h_str = "\r\nProxy-Connection: close"; 
-	    strncpy(l_e3, h_str,strlen(h_str));
 #ifdef DEBUG
-	    printf("DEBUG:after adding p-c:c is:%s\n",request);
-#endif
-	  }else{
-#ifdef DEBUG
-	    printf("DEBUG: Has p-c header field, no need to be added\n");
-#endif
-	  }
-	}
-	char* l_e4 = request + strlen(request);
-	if(!l_e4){
-	  printf("request buffer ran out\n");
-	  return NULL;
-	}else{
-	  char* hs_in_req = strstr(request,"\r\n");
-	  char* ua_s = strstr(hs_in_req, "User-Agent:");
-	  //   printf("DEBUG: u_a is:%s\n",u_a);
-	  if(!ua_s){
-	    char* h_str = "\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3)";//\n  Gecko/20120305 Firefox/10.0.3";  
-#ifdef DEBUG
-	    printf("DEBUG: No User-Agent header field, need to be added as following\n");
-	    printf("User-Agent: xxx\n");
-#endif
-	    strncpy(l_e4,h_str,strlen(h_str));
-#ifdef DEBUG
-	    printf("after adding u-a is:%s\n",request);
-#endif
-	  }else{
-#ifdef DEBUG
-	    printf("DEBUG: Has u-a header field, no need to be added\n");
-#endif
-	  }
-	}
-	char* l_e5 = request+ strlen(request);
-	char* body_s = strstr(buffer,"\r\n\r\n");
-	char* body_e = buffer + strlen(buffer);
-	printf("DEBUG:body_e is:%s\n",body_e);
-	if(!body_e){
-	  printf("buffer end with null\n");
-	  return NULL;
-	}else{
-	  strncpy(l_e5,body_s,(int)(body_e - body_s));
-#ifdef DEBUG
-	  printf("After copy body, request is:%s\n",request);
+	  printf("DEBUG: Has p-c header field, no need to be added\n");
 #endif
 	}
       }
-    }  
+      char* l_e4 = request + strlen(request);
+      if(!l_e4){
+	printf("request buffer ran out\n");
+	return NULL;
+      }else{
+	char* hs_in_req = strstr(request,"\r\n");
+	char* ua_s = strstr(hs_in_req, "User-Agent:");
+	//   printf("DEBUG: u_a is:%s\n",u_a);
+	if(!ua_s){
+	  char* h_str = "\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3)";//\n  Gecko/20120305 Firefox/10.0.3";  
 #ifdef DEBUG
-      printf("after rewrite, return as:\n%s\n", request);
+	  printf("DEBUG: No User-Agent header field, need to be added as following\n");
+	  printf("User-Agent: xxx\n");
 #endif
-      
-      free(tokens->c_url);
-      free(tokens->p_url);
-      free(tokens->host);
-      
-      return request;
+	  strncpy(l_e4,h_str,strlen(h_str));
+#ifdef DEBUG
+	  printf("after adding u-a is:%s\n",request);
+#endif
+	}else{
+#ifdef DEBUG
+	  printf("DEBUG: Has u-a header field, no need to be added\n");
+#endif
+	}
+      }
+      char* l_e5 = request+ strlen(request);
+      char* body_s = strstr(buffer,"\r\n\r\n");
+      char* body_e = buffer + strlen(buffer);
+      printf("DEBUG:body_e is:%s\n",body_e);
+      if(!body_e){
+	printf("buffer end with null\n");
+	return NULL;
+      }else{
+	strncpy(l_e5,body_s,(int)(body_e - body_s));
+#ifdef DEBUG
+	printf("After copy body, request is:%s\n",request);
+#endif
+      }
+    }
+  }  
+#ifdef DEBUG
+  printf("after rewrite, return as:\n%s\n", request);
+#endif
+  
+  //  free(tokens->c_url);
+  // free(tokens->p_url);
+  // free(tokens->host);
+  
+  return request;
 }
 
 
